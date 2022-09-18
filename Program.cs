fa791f9e724e1dae4e27421fe460130e9bf76245
Gendrin <Gendrinvl@gmail.com>
2022-09-18 20:32:08 +1000

    final modification

--- Program.cs
+++ Program.cs
@@ -1,41 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 string[] inputString;
-int allCountSmallStrings=0;
-Console.WriteLine("Please enter strings through whitespase!");
-inputString = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-Console.Write("[");
-for (int i = 0; i < inputString.Length; i++)
-{
-    if (inputString[i].Length < 4)
+Console.WriteLine("Please enter long checked string?");
+
+if (int.TryParse(Console.ReadLine(), out int longString))
     {
-        allCountSmallStrings++;
+        if (longString > 0)
+        {
+        Console.WriteLine("Please enter strings through whitespase!");
+        inputString = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        PrintString(inputString);
+        Console.Write(" -> ");
+        PrintString(CheckCountForSimbols(inputString, longString+1));
     }
-    if (i < inputString.Length - 1)
-    {
-        Console.Write($"\"{inputString[i]}\",");
-    }
-    else Console.Write($"\"{inputString[i]}\"");
+        else Console.WriteLine("No correct enters!");
 }
-Console.Write("]");
-
-string[] arrRezStrings = new string[allCountSmallStrings];
-
-
-Console.Write(" -> [");
-for (int j=0,i = 0; i < inputString.Length; i++)
-{
-    if (inputString[i].Length < 4)
-    {
-        arrRezStrings[j] = inputString[i];
-        if (j < arrRezStrings.Length-1)
-            {
-            Console.Write($"\"{arrRezStrings[j]}\",");
-            j++;
-            }
-        else Console.Write($"\"{arrRezStrings[j]}\"");
-    }
-}
-Console.Write("]");
 
 void PrintString(string[] printStrings)
 {
@@ -53,6 +31,7 @@ void PrintString(string[] printStrings)
 
 string[] CheckCountForSimbols(string[] checkString, int longString)
 {
+    int allCountSmallStrings=0;
     for (int i = 0; i < checkString.Length; i++)
     {
         if (checkString[i].Length < longString)
